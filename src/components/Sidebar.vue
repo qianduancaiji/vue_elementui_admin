715757ce0de51e35cc241bf450fec03ad1e27d36
孔雪阳 <992183887@qq.com>
2019-04-08 23:32:06 +0800

    改进路由

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,7 +2,7 @@
     <div class="sidebar">
         <el-menu class="sidebar-el-menu" :default-active="onRoutes" :collapse="collapse" background-color="#324157"
             text-color="#bfcbd9" active-text-color="#20a0ff" unique-opened router>
-            <template v-for="item in items">
+            <!-- <template v-for="item in items">
                 <template v-if="item.subs">
                     <el-submenu :index="item.index" :key="item.index">
                         <template slot="title">
@@ -26,6 +26,31 @@
                         <i :class="item.icon"></i><span slot="title">{{ item.title }}</span>
                     </el-menu-item>
                 </template>
+            </template> -->
+            <template v-for="item in routes">
+                <template v-if="item.children">
+                    <el-submenu :index="item.path" :key="item.path">
+                        <template slot="title">
+                            <i :class="item.meta.icon"></i><span slot="title">{{ item.meta.title }}</span>
+                        </template>
+                        <template v-for="subItem in item.children">
+                            <el-submenu v-if="subItem.children" :index="subItem.path" :key="subItem.path">
+                                <template slot="title">{{ subItem.meta.title }}</template>
+                                <el-menu-item v-for="(threeItem,i) in subItem.children" :key="i" :index="threeItem.path">
+                                    {{ threeItem.meta.title }}
+                                </el-menu-item>
+                            </el-submenu>
+                            <el-menu-item v-else :index="subItem.path" :key="subItem.path">
+                                {{ subItem.meta.title }}
+                            </el-menu-item>
+                        </template>
+                    </el-submenu>
+                </template>
+                <template v-else>
+                    <el-menu-item :index="item.path" :key="item.path">
+                        <i :class="item.meta.icon"></i><span slot="title">{{ item.meta.title }}</span>
+                    </el-menu-item>
+                </template>
             </template>
         </el-menu>
     </div>
@@ -36,101 +61,25 @@
     export default {
         data() {
             return {
-                collapse: false,
-                items: [
-                    {
-                        icon: 'el-icon-lx-home',
-                        index: 'dashboard',
-                        title: '系统首页'
-                    },
-                    {
-                        icon: 'el-icon-lx-cascades',
-                        index: 'table',
-                        title: '基础表格'
-                    },
-                    {
-                        icon: 'el-icon-lx-copy',
-                        index: 'tabs',
-                        title: 'tab选项卡'
-                    },
-                    {
-                        icon: 'el-icon-lx-calendar',
-                        index: '3',
-                        title: '表单相关',
-                        subs: [
-                            {
-                                index: 'form',
-                                title: '基本表单'
-                            },
-                            {
-                                index: '3-2',
-                                title: '三级菜单',
-                                subs: [
-                                    {
-                                        index: 'editor',
-                                        title: '富文本编辑器'
-                                    },
-                                    {
-                                        index: 'markdown',
-                                        title: 'markdown编辑器'
-                                    },
-                                ]
-                            },
-                            {
-                                index: 'upload',
-                                title: '文件上传'
-                            }
-                        ]
-                    },
-                    {
-                        icon: 'el-icon-lx-emoji',
-                        index: 'icon',
-                        title: '自定义图标'
-                    },
-                    {
-                        icon: 'el-icon-lx-favor',
-                        index: 'charts',
-                        title: 'schart图表'
-                    },
-                    {
-                        icon: 'el-icon-rank',
-                        index: '6',
-                        title: '拖拽组件',
-                        subs: [
-                            {
-                                index: 'drag',
-                                title: '拖拽列表',
-                            },
-                            {
-                                index: 'dialog',
-                                title: '拖拽弹框',
-                            }
-                        ]
-                    },
-                    {
-                        icon: 'el-icon-lx-warn',
-                        index: '7',
-                        title: '错误处理',
-                        subs: [
-                            {
-                                index: 'permission',
-                                title: '权限测试'
-                            },
-                            {
-                                index: '404',
-                                title: '404页面'
-                            }
-                        ]
-                    }
-                ]
+                collapse: false
             }
         },
         computed:{
             onRoutes(){
                 return this.$route.path.replace('/','');
+            },
+            routes() {
+                let routes = []
+                this.$router.options.routes.forEach(item => {
+                    if (item.path === '/' && !item.hidden && item.children.length !== 0) {
+                        routes.push(...item.children)
+                    }
+                })
+                return routes
             }
         },
         created(){
+            
             // 通过 Event Bus 进行组件间通信，来折叠侧边栏
             bus.$on('collapse', msg => {
                 this.collapse = msg;
--- src/router/admin.js
+++ src/router/admin.js
@@ -1,104 +1,194 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-
+import RouterView from '../components/RouterView.vue';
+import Home from '../components/Home.vue' 
 Vue.use(Router);
 
 export default new Router({
     routes: [
         {
             path: '/',
+            hidden: true,
             redirect: '/dashboard'
         },
         {
             path: '/',
-            component: resolve => require(['../components/Home.vue'], resolve),
-            meta: { title: '自述文件' },
+            component: Home,
+            meta: {
+                title: '容器'
+            },
             children:[
                 {
                     path: '/dashboard',
-                    component: resolve => require(['../views/page/Dashboard.vue'], resolve),
-                    meta: { title: '系统首页' }
+                    component: () => import('../views/page/Dashboard.vue'),
+                    meta: {
+                        title: '系统首页',
+                        icon: 'el-icon-lx-home'
+                    }
                 },
                 {
                     path: '/icon',
-                    component: resolve => require(['../views/page/Icon.vue'], resolve),
-                    meta: { title: '自定义图标' }
+                    component: () => import('../views/page/Icon.vue'),
+                    meta: {
+                        title: '自定义图标',
+                        icon: 'el-icon-lx-emoji'
+                    }
                 },
                 {
                     path: '/table',
-                    component: resolve => require(['../views/page/BaseTable.vue'], resolve),
-                    meta: { title: '基础表格' }
+                    component: () => import('../views/page/BaseTable.vue'),
+                    meta: {
+                        title: '基础表格',
+                        icon: 'el-icon-lx-cascades'
+                    }
                 },
                 {
                     path: '/tabs',
-                    component: resolve => require(['../views/page/Tabs.vue'], resolve),
-                    meta: { title: 'tab选项卡' }
+                    component: () => import('../views/page/Tabs.vue'),
+                    meta: {
+                        title: 'tab选项卡',
+                        icon: 'el-icon-lx-copy'
+                    }
                 },
                 {
                     path: '/form',
-                    component: resolve => require(['../views/page/BaseForm.vue'], resolve),
-                    meta: { title: '基本表单' }
+                    component: RouterView,
+                    redirect: '/404',
+                    meta: {
+                        title: '表单相关',
+                        icon: 'el-icon-lx-calendar'
+                    },
+                    children: [
+                        {
+                            path: '/form/form',
+                            component: () => import('../views/page/BaseForm.vue'),
+                            meta: {
+                                title: '基本表单',
+                                icon: 'el-icon-lx-emoji'
+                            }
+                        },
+                        {
+                            path: '/form/edit',
+                            component: RouterView,
+                            redirect: '/404',
+                            meta: {
+                                title: '富文本',
+                                icon: 'el-icon-lx-emoji'
+                            },
+                            children: [
+                                {
+                                    path: '/form/edit/editor',
+                                    component: () => import('../views/page/VueEditor.vue'),
+                                    meta: {
+                                        title: '富文本编辑器',
+                                        icon: 'el-icon-lx-editor'
+                                    }
+                                },
+                                {
+                                    path: '/form/edit/markdown',
+                                    component: () => import('../views/page/Markdown.vue'),
+                                    meta: {
+                                        title: 'markdown编辑器',
+                                        icon: 'el-icon-lx-markdown'
+                                    }    
+                                }
+                            ]
+                        },
+                        {
+                            path: '/form/upload',
+                            component: () => import('../views/page/Upload.vue'),
+                            meta: {
+                                title: '文件上传',
+                                icon: 'el-icon-lx-emoji'
+                            }   
+                        }
+                    ]
                 },
-                {
-                    // 富文本编辑器组件
-                    path: '/editor',
-                    component: resolve => require(['../views/page/VueEditor.vue'], resolve),
-                    meta: { title: '富文本编辑器' }
-                },
-                {
-                    // markdown组件
-                    path: '/markdown',
-                    component: resolve => require(['../views/page/Markdown.vue'], resolve),
-                    meta: { title: 'markdown编辑器' }    
-                },
-                {
-                    // 图片上传组件
-                    path: '/upload',
-                    component: resolve => require(['../views/page/Upload.vue'], resolve),
-                    meta: { title: '文件上传' }   
-                },
+                
                 {
                     // vue-schart组件
                     path: '/charts',
-                    component: resolve => require(['../views/page/BaseCharts.vue'], resolve),
-                    meta: { title: 'schart图表' }
+                    component: () => import('../views/page/BaseCharts.vue'),
+                    meta: {
+                        title: 'schart图表',
+                        icon: 'el-icon-lx-favor'
+                    }
                 },
                 {
-                    // 拖拽列表组件
-                    path: '/drag',
-                    component: resolve => require(['../views/page/DragList.vue'], resolve),
-                    meta: { title: '拖拽列表' }
-                },
-                {
-                    // 拖拽Dialog组件
-                    path: '/dialog',
-                    component: resolve => require(['../views/page/DragDialog.vue'], resolve),
-                    meta: { title: '拖拽弹框' }
+                    path: '/tuo',
+                    component: RouterView,
+                    meta: {
+                        title: '拖拽组件',
+                        icon: 'el-icon-lx-rank'
+                    },
+                    children: [
+                        {
+
+                            path: '/tuo/drag',
+                            component: () => import('../views/page/DragList.vue'),
+                            meta: {
+                                title: '拖拽列表',
+                                icon: 'el-icon-lx-rank'
+                            }
+                        },
+                        {
+
+                            path: '/tuo/dialog',
+                            component: () => import('../views/page/DragDialog.vue'),
+                            meta: {
+                                title: '拖拽弹框',
+                                icon: 'el-icon-lx-dialog'
+                            }
+                        }
+                    ]
                 },
                 {
                     // 权限页面
                     path: '/permission',
-                    component: resolve => require(['../views/page/Permission.vue'], resolve),
-                    meta: { title: '权限测试', permission: true }
+                    component: () => import('../views/page/Permission.vue'),
+                    meta: {
+                        title: '权限测试',
+                        icon: 'el-icon-lx-warn',
+                        permission: true
+                    }
                 },
                 {
-                    path: '/404',
-                    component: resolve => require(['../views/page/404.vue'], resolve),
-                    meta: { title: '404' }
-                },
-                {
-                    path: '/403',
-                    component: resolve => require(['../views/page/403.vue'], resolve),
-                    meta: { title: '403' }
+                    path: '/error',
+                    component: RouterView,
+                    meta: {
+                        title: '错误处理',
+                        icon: 'el-icon-lx-warn'
+                    },
+                    children: [
+                        {
+                            path: '/error/404',
+                            component: () => import('../views/page/404.vue'),
+                            meta: {
+                                title: '404',
+                                icon: 'el-icon-lx-emoji'
+                            }
+                        },
+                        {
+                            path: '/error/403',
+                            component: () => import('../views/page/403.vue'),
+                            meta: {
+                                title: '403',
+                                icon: 'el-icon-lx-emoji'
+                            }
+                        }
+                    ]
+
                 }
             ]
         },
         {
             path: '/login',
-            component: resolve => require(['../views/page/Login.vue'], resolve)
+            hidden: true,
+            component: () => import('../views/page/Login.vue')
         },
         {
             path: '*',
+            hidden: true,
             redirect: '/404'
         }
     ]
